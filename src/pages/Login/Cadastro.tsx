8887a87fccd58d10010187c03adceb44bb8f03f1
gabriel bibiano <gabriel.bibiano@outlook.com>
2022-08-04 19:23:54 -0300

    Co-authored-by: João Bibiano <jv.bibiano@hotmail.com>

--- src/pages/Login/Cadastro.tsx
+++ src/pages/Login/Cadastro.tsx
@@ -1,22 +1,47 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
+import { Api } from "../../services/Api";
+import { useNavigate } from "react-router-dom";
+
 export const Cadastro = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    Api.post("/user", {
+      name: name,
+      email: email,
+    }).then(() => {
+      navigate("/login");
+    });
+  };
+
   return (
     <div>
       <Header groupType="logo" />
 
       <div className="px-5 flex h-screen justify-center items-center">
-        <div className="max-w-xl">
+        <form className="max-w-xl" onSubmit={onSubmitForm}>
           <input
             type="text"
             className="mt-3 w-full h-10 text-base border p-6 rounded-full shadow-xl"
             placeholder="Nome"
+            required
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
           <input
-            type="text"
             className=" mt-3 w-full h-10 text-base border p-6 rounded-full shadow-xl"
             placeholder="E-mail"
+            required
+            value={email}
+            type="email"
+            onChange={(event) => setEmail(event.target.value)}
           />
-          <input
+          {/* <input
             type="text"
             className="mt-3 w-full h-10 text-base border p-6 rounded-full shadow-xl"
             placeholder="Senha"
@@ -25,13 +50,12 @@ export const Cadastro = () => {
             type="text"
             className=" mt-3 w-full h-10 text-base border p-6 rounded-full shadow-xl"
             placeholder="Confirmação Senha"
-          />
-          <a href="/Login">
-            <button className="mt-8 w-full h-10 text-lg text-white font-semibold border rounded-full shadow-xl bg-blue-600 hover:bg-blue-500">
-              Cadastrar
-            </button>
-          </a>
-        </div>
+          /> */}
+
+          <button className="mt-8 w-full h-10 text-lg text-white font-semibold border rounded-full shadow-xl bg-blue-600 hover:bg-blue-500">
+            Cadastrar
+          </button>
+        </form>
       </div>
     </div>
   );
--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,5 +1,14 @@
 import Axios from "axios";
 
 export const Api = Axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL: "http://localhost:3333",
 });
+
+/*Api.post(
+  "/product", {
+    "description": "Brinco branco",
+    "custo": 16,
+    "estoque": 2,
+    "preco": 28
+}
+)*/
